354f9f9c1cc1064fe760180ae1606ce53d90da5d
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2024-01-12 19:59:36 -0600

    Fix autonomousInit javadoc

--- simple/src/main/java/frc/robot/Robot.java
+++ simple/src/main/java/frc/robot/Robot.java
@@ -76,23 +76,7 @@ public class Robot extends TimedRobot {
     m_lineSensor.update(true);
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different
-   * autonomous modes using the dashboard. The sendable chooser code works with
-   * the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
-   * chooser code and
-   * uncomment the getString line to get the auto name from the text box below the
-   * Gyro
-   *
-   * <p>
-   * You can add additional auto modes by adding additional comparisons to the
-   * switch structure
-   * below with additional strings. If using the SendableChooser make sure to add
-   * them to the
-   * chooser code above as well.
-   */
+  /** This function is called once when autonomous is enabled. */
   @Override
   public void autonomousInit() {
     m_drivetrain.resetEncoders();
